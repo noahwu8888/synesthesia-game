8008149c93883aa2ddf6abe6316b43b332ad485a
Preston20050813 <80573207+Preston20050813@users.noreply.github.com>
2023-10-08 18:26:43 -0400

    moves from the start position when reaches end position

--- Synesthesia Game Godot/Scenes/Tests/Music Bar.gd
+++ Synesthesia Game Godot/Scenes/Tests/Music Bar.gd
@@ -2,11 +2,12 @@ extends Area2D
 
 
 @export var direction : Vector2
+@export var move_speed : float = 30.0
 @export var beatsPerMinute = 0
 @export var beatsPerMeasure = 0
 @export var measuresPerLoop = 0
 
-var move_speed : float = ((60 / beatsPerMinute) * beatsPerMeasure * (measuresPerLoop))
+#var move_speed : float = ((60 / beatsPerMinute) * beatsPerMeasure * (measuresPerLoop))
 var start_pos : Vector2
 var target_pos : Vector2
 # Called when the node enters the scene tree for the first time.
@@ -21,7 +22,9 @@ func _process(delta):
 	global_position = global_position.move_toward(target_pos, move_speed * delta)
 
 	if global_position == target_pos:
-		if global_position == start_pos:
-			target_pos = start_pos + direction
-		else:
-			target_pos = start_pos 
+		global_position = start_pos
+		
+		#if global_position == start_pos:
+			#target_pos = start_pos + direction
+		#else:
+			#target_pos = start_pos 
