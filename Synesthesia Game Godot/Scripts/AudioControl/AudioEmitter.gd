23ed4214f00339c488b8423d85bceb467b9f3e73
noahwu8888 <noahwu8888@gmail.com>
2023-10-09 05:33:48 +0800

    Testing for Music Bar

--- Synesthesia Game Godot/Scripts/AudioControl/AudioEmitter.gd
+++ Synesthesia Game Godot/Scripts/AudioControl/AudioEmitter.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
 @export var event: EventAsset
+var instance: EventInstance
 
 func _ready():
-	pass	
+	instance = RuntimeManager.create_instance(event)
+
+
+func _on_area_2d_body_entered(_body):
+	instance.start()
+	instance.release()
