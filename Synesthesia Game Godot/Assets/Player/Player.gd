640803e9c53c295c1262de95a34abafff2a2c7d5
Kenji <shorekenji@gmail.com>
2023-10-08 18:23:15 -0400

    change player movement

--- Synesthesia Game Godot/Assets/Player/Player.gd
+++ Synesthesia Game Godot/Assets/Player/Player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -9,6 +8,12 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
+	self.Sprite
+	
+	var collision = $WallArea/CollisionShape2D
+	
+	move_and_slide()
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -26,3 +31,10 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	var moo = $WallArea
+	moo.get_overlapping_bodies()
+	
+
+
+func _on_wall_area_body_entered(body):
+	pass # Replace with function body.
