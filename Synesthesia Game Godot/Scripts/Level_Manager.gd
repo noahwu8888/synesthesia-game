732d9778b931eadc1f3e6f3ded3f4d50c600a350
noahwu8888 <noahwu8888@gmail.com>
2023-11-06 07:23:00 +0800

    Portals work kinda

--- Synesthesia Game Godot/Scripts/Level_Manager.gd
+++ Synesthesia Game Godot/Scripts/Level_Manager.gd
@@ -4,6 +4,7 @@ extends Node
 var rooms
 @onready var camera = $Camera
 @onready var player = $Player
+static var current_room = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,9 +13,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		move_to_room(1)
-	print(camera.position.x)
+	pass
+	
+
 func move_to_room(room_number):
 	var room = self.rooms[room_number]
 	self.camera.new_export_vars(0, room.left_bound, room.right_bound)
+	current_room = room_number
--- Synesthesia Game Godot/Scripts/Level_Manager/Room_Script.gd
+++ Synesthesia Game Godot/Scripts/Level_Manager/Room_Script.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@export var current_room = 0
+
 @export var transition_timer = 0.0
 @export var left_bound = 0
 @export var right_bound = 0
@@ -16,3 +18,9 @@ extends Node
 @export var max_change_in_speed = 0.0
 @export var look_ahead = 0
 @export var vertical_pan_speed = 0.0
+
+
+var root_node
+
+func _ready():
+	root_node = get_tree().root.get_child(0)
